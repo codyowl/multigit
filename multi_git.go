ce5de1afd0a16ee8533ec4d89712ea27e2e3211d
codyowl <codyowl@gmail.com>
2024-12-23 00:38:33 +0530

    a simple menu added

--- multi_git.go
+++ multi_git.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
+	"github.com/nexidian/gocliselect"
+	"golang.org/x/sys/windows"
 )
 
 const (
 	git_config_list_command = "git config --list"
 )
+
+
+// main menu 
+func simpleMenu() string {
+	options := []string{"list", "active", "add", "remove"}
+	fmt.Println("What would you like to do?")
+	for i, option := range options {
+		fmt.Printf("%d. %s\n", i+1, option)
+	}
+
+	var choice int
+	fmt.Scan(&choice)
+
+	if choice < 1 || choice > len(options) {
+		fmt.Println("Invalid choice. Exiting.")
+		os.Exit(1)
+	}
+	return options[choice-1]
+}
+
+
 
 
 func get_git_details(){
@@ -34,5 +58,7 @@ func get_git_details(){
 }
 
 func main(){
+	choice := simpleMenu()
+	fmt.Printf("Choice: %s\n", choice)
 	get_git_details()
 }
