2890bd92ea173f43a0b8295e3cf6161ace4d353c
codyowl <codyowl@gmail.com>
2025-01-08 23:16:21 +0530

    add profile added to get inputs and add it to a file

--- multi_git.go
+++ multi_git.go
@@ -7,6 +7,7 @@ import (
 	"os/user"
 	"strings"
 	"path/filepath"
+	"bufio"
 )
 
 const (
@@ -74,6 +75,36 @@ func getActiveProfileDetail(){
 
 func addProfile(){
 	fmt.Println("Add a new username and email to the list")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter username: ")
+	username, _ := reader.ReadString('\n')
+	
+
+	fmt.Println("Enter user.email: ")
+	email, _ := reader.ReadString('\n')
+
+	// getting documents home folder path to append the file
+	documentsFolder := getDocumentsPath()
+	profilesFile := filepath.Join(documentsFolder, "multigit_profiles.txt")
+	
+	// appending data to a file
+	file, err := os.OpenFile(profilesFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+
+
+	// writing the data 
+	_, err = file.WriteString(fmt.Sprintf("username=%s, user.email=%s\n", username, email))
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Profile added successfully!")
 }
 
 func removeProfile(){
